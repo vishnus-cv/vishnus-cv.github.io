f5d24014d7586b137ef7db6edc113878e43448b0
Vishnu Satheesan <vishnus.cv@gmail.com>
2023-05-26 17:18:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { google } = require("googleapis");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", async (req, res) => {
+  const { name, mobile } = req.body;
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  const client = await auth.getClient();
+
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1qYhj7b9QHkKGpZrJbOrIlvsbi5bo-nAlPz2gBgPFZbs";
+
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:B",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[name, mobile]],
+    },
+  });
+
+  res.send("Data submitted successfully!");
+});
+
+app.listen(8083, (req, res) => console.log("running on 8083"));
